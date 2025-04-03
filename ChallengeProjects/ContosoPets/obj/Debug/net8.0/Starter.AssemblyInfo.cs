5a74751ca78d7ffbd0e9dacdefc38517982c658c
Erasmo Ribeiro Bezerra <erasmo.ads.tech@gmail.com>
2025-04-03 00:10:10 -0300

    add CreateCSharpMethods

--- ChallengeProjects/ContosoPets/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ ChallengeProjects/ContosoPets/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e7078bffa6cda8a84a63851b297f9f68bdb3075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75207abeb4359a93b69070e4b57b7caff81803c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
