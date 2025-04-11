b81fb3463e770f2e4fba11ed8d7d8437d20ff496
Erasmo Ribeiro Bezerra <erasmo.ads.tech@gmail.com>
2025-04-11 18:00:40 -0300

    add Debugging and Exceptions

--- DebuggingAndExceptionHandling/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
-
-int studentCount = students.Length;
-
-Console.WriteLine("The final name is: " + students[studentCount]);
